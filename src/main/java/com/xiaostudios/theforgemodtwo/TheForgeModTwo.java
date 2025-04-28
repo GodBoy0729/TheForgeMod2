a0c234017f5db33ed330558929aecc8bad34edf4
GodBoy0729 <95355639+GodBoy0729@users.noreply.github.com>
2025-04-28 14:03:43 +0800

    second commit for testing purposes

--- src/main/java/com/xiaostudios/theforgemodtwo/TheForgeModTwo.java
+++ src/main/java/com/xiaostudios/theforgemodtwo/TheForgeModTwo.java
@@ -14,6 +14,8 @@ import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
+//This is the main file of the forge mod
+
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(TheForgeModTwo.MOD_ID)
 public class TheForgeModTwo {
